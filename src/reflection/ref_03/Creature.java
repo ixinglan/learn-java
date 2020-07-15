1b102b71f230b4d61a79d11d010b593288da47d1
赵建强 <13269982081@163.com>
2020-07-15 08:07:14 +0800

    reflection

---
+++ src/reflection/ref_03/Creature.java
@@ -0,0 +1,17 @@
+package reflection.ref_03;
+
+import java.io.Serializable;
+
+public class Creature<T> implements Serializable {
+    private char gender;
+    public double weight;
+
+    private void breath() {
+        System.out.println("爱是一道光");
+    }
+
+    public void eat() {
+        System.out.println("吃点东西吧");
+    }
+
+}
---
+++ src/reflection/ref_03/MyInterface.java
@@ -0,0 +1,5 @@
+package reflection.ref_03;
+
+public interface MyInterface {
+    void info();
+}
